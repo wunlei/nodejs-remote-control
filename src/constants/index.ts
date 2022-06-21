8d2a81e949f3bedbe2b518f044c16af5d3f9d107
wunlei <confusioncosmos@gmail.com>
2022-06-21 20:18:31 +0300

    feat: implement ws server

---
+++ src/constants/index.ts
@@ -0,0 +1,2 @@
+export const HTTP_PORT_DEFAULT = 3000;
+export const WS_PORT_DEFAULT = 8080;
---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import "dotenv/config";
+import { httpServer } from "./http_server/index";
+import WebSocket, { WebSocketServer } from "ws";
+import { HTTP_PORT_DEFAULT, WS_PORT_DEFAULT } from "./constants/index";
+
+const HTTP_PORT = process.env.HTTP_PORT || HTTP_PORT_DEFAULT;
+const WS_PORT = Number(process.env.WS_PORT) || WS_PORT_DEFAULT;
+
+console.log(`Start static http server on http://localhost:${HTTP_PORT}`);
+
+httpServer.listen(HTTP_PORT);
+
+export const wsServer = new WebSocketServer({ port: WS_PORT });
+
+wsServer.on("connection", function connection(ws) {
+  console.log("Client connected");
+
+  ws.on("close", () => {
+    console.log("Connection closed");
+  });
+
+  ws.on("error", (error) => {
+    console.error(error);
+  });
+});
+
+wsServer.on("error", (error) => {
+  console.error(error);
+});
+
+wsServer.on("close", () => {
+  console.log("Server closed");
+});
+
+process.on("SIGINT", () => {
+  wsServer.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.close();
+    }
+  });
+
+  wsServer.close();
+});
