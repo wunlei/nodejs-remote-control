bfc7a99af5ae40233aeb64a940914e8a5e04ba0e
wunlei <confusioncosmos@gmail.com>
2022-06-21 22:07:55 +0300

    feat: implement mouse commands

--- src/ws_server/index.ts
+++ src/ws_server/index.ts
@@ -1,4 +1,5 @@
 import { createWebSocketStream, WebSocket } from "ws";
+import { mouseAction, mouseCommands } from "./components/mouse";
 
 export function wsStream(ws: WebSocket) {
   const duplex = createWebSocketStream(ws, {
@@ -7,10 +8,13 @@ export function wsStream(ws: WebSocket) {
   });
 
   duplex.on("data", function (data: string) {
-    console.log(data);
     const parsedData = data.split(" ");
     const command = parsedData[0];
     const commandArgs = parsedData.slice(1).map((el) => parseInt(el, 10));
+
+    if (mouseCommands.includes(command)) {
+      mouseAction(command, commandArgs, duplex);
+    }
   });
 
   duplex.on("error", (error) => {
