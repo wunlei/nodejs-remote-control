7884023c0b7309814da09052c3dfe6b3436e317a
wunlei <confusioncosmos@gmail.com>
2022-06-23 15:40:03 +0300

    refactor: fix logging

--- src/ws_server/components/Mouse.ts
+++ src/ws_server/components/Mouse.ts
@@ -18,27 +18,27 @@ export function mouseAction(
     const mouse = getMousePos();
     if (command === "mouse_position") {
       duplex.write(`${command} ${mouse.x},${mouse.y}\0`);
-      console.log(`Success: ${command} ${mouse.x},${mouse.y}\0`);
+      console.log(`Success: ${command} ${mouse.x},${mouse.y}`);
     } else if (command === "mouse_up") {
       const offset = commandArgs[0];
       moveMouse(mouse.x, mouse.y - offset);
       duplex.write(`${command}\0`);
-      console.log(`Success: ${command}`);
+      console.log(`Success: ${command} ${offset}`);
     } else if (command === "mouse_down") {
       const offset = commandArgs[0];
       moveMouse(mouse.x, mouse.y + offset);
       duplex.write(`${command}\0`);
-      console.log(`Success: ${command}`);
+      console.log(`Success: ${command} ${offset}`);
     } else if (command === "mouse_left") {
       const offset = commandArgs[0];
       moveMouse(mouse.x - offset, mouse.y);
       duplex.write(`${command}\0`);
-      console.log(`Success: ${command}`);
+      console.log(`Success: ${command} ${offset}`);
     } else if (command === "mouse_right") {
       const offset = commandArgs[0];
       moveMouse(mouse.x + offset, mouse.y);
       duplex.write(`${command}\0`);
-      console.log(`Success: ${command}`);
+      console.log(`Success: ${command} ${offset}`);
     }
   } catch (error) {
     if (error instanceof Error) {
